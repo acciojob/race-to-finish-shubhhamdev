7f03e6cb160a65f1fd966b35d2ed562b3e22d0c0
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-08-08 22:38:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 window.promises = [];
+// Add your promises to the array 'promises'
+for (let i = 0; i < 5; i++) {
+    promises.push(new Promise((resolve) => {
+        const randomTime = getRandomTime(1000, 5000);
+        setTimeout(() => {
+            resolve(`Promise ${i + 1} resolved after ${randomTime / 1000} seconds`);
+        }, randomTime);
+    }));
+}
 
 // Do not change the code above this
 // add your promises to the array `promises`
